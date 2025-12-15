45d7a39cf9fd46fb69e526988ddc5302edd7b10e
vishal27shetty <vishal27shetty@gmail.com>
2025-12-15 22:52:21 +0530

    refactor: created a separate json dummy data

--- agent.py
+++ agent.py
@@ -5,30 +5,25 @@ from livekit.agents import AgentServer,AgentSession, Agent, room_io, function_to
 from livekit.plugins import noise_cancellation, silero
 from livekit.plugins.turn_detector.multilingual import MultilingualModel
 
+import json
+
 load_dotenv(".env")
 
-data = {
-    "vishal" : {
-        "email" : "vishalshetty@gmail.com",
-        "salary" : 2000
-    } ,
-    "tom" : {
-        "email" : "tom@gmail.com",
-        "salary" : 20000
-    } ,
-}
+with open('data.json', 'r') as file:
+    data = json.load(file)
 
-
+print(data)
+print(type(data))
 class Assistant(Agent):
     def __init__(self) -> None:
         super().__init__(
             instructions="""You are a helpful voice AI assistant.
-            You eagerly assist users with their questions by providing information from your extensive knowledge.
+            You eagerly assist users with their questions by providing information from your extensive knowledge of different employees.
             Your responses are concise, to the point, and without any complex formatting or punctuation including emojis, asterisks, or other symbols.
             You are curious, friendly, and have a sense of humor. If the user asks about employee details such as email,
-            you must use the employee directory tool instead of guessing.
-            Dont give salary or any confidental information about the user only provide the contact details
-            
+            You must use the employee directory tool for finding employee information instead of guessing.
+            You must ask for the last name if the user only tells the first name and the retrival doesnt happen.
+            Dont give salary or any confidental information about the user only provide the contact details.
             """,
         )
     @function_tool
@@ -37,20 +32,18 @@ class Assistant(Agent):
         Look up employee contact details.
 
         Use this tool when the user asks for an employee's email.
-        The input is the employee's first name.
-        Ensure you conver the name to lowercase and remove the apostrophe s
+        The input is the employee's first name and last name which is not separeted with a whitespace.
+        Ensure you conver the name to lowercase and remove the apostrophe S.
         
         Args:
             employee_name: Name of the employee like John
         """
-
-        if data.get(employee_name):
+        employee_name = employee_name.lower().strip()
+        print(employee_name)
+        if data[employee_name]:
             return data[employee_name]
         else:
             return "Employee doesnt exist in the directory"
-
-
-        
 
 server = AgentServer()
 
